8ccbf3c6a3f0fbcc9f77459ede5b4eee1df38230
uriel-martinez1 <martinezu444@gmail.com>
2024-04-03 22:14:31 -0400

    Latest version of app.js

--- 000-practice-code/advice-generator-app/src/App.js
+++ 000-practice-code/advice-generator-app/src/App.js
@@ -3,4 +3,9 @@ import { useEffect } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
+
+  // we do async function to will make the API call
+  async function getAdvice() {
+
+  }
 }
