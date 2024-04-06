5ec2097475dc0ad4e4c8c241a194d47a7d6edaf2
uriel-martinez1 <martinezu444@gmail.com>
2024-04-05 21:14:05 -0400

    Was able to fetch and showcase data from advice api

--- 000-practice-code/advice-generator-app/src/App.js
+++ 000-practice-code/advice-generator-app/src/App.js
@@ -2,10 +2,38 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function App() {
+  const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
 
   // we do async function to will make the API call
   async function getAdvice() {
-
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    console.log(data);
   }
+
+  // we will useEffect that will call the function getAdvice
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>Advice Generator</h1>
+      <h2>{advice}</h2>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+// we will create a component - they are called props in React
+function Message(props){
+  return (
+    <div>
+      <p>
+        You have clicked the button <strong>{props.count}</strong> times
+      </p>
+    </div>
+  );
 }
